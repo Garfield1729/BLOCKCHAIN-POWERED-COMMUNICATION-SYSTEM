2559fc00d4a1e2ae7c4687a14b51ddb568058b5f
naveen <n.a.v.e.e.n.b.a.b.u.0.1.0.1.0.1@gmail.com>
2026-02-11 11:45:14 +0530

    final commit

---
+++ Components/NavBar/NavBar.jsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useState, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+//INTERNAL IMPORT
+import Style from "./NavBar.module.css";
+import { ChatAppContect } from "../../Context/ChatAppContext";
+import { Model, Error } from "../index";
+import images from "../../assets";
+
+const NavBar = () => {
+  const menuItems = [
+    // {
+    //   menu: "All Users",
+    //   link: "alluser",
+    // },
+    // {
+    //   menu: "CHAT",
+    //   link: "/",
+    // },
+    // {
+    //   menu: "CONTACT",
+    //   link: "/",
+    // },
+    // {
+    //   menu: "SETTING",
+    //   link: "/",
+    // },
+  ];
+
+  //USESTATE
+  const [active, setActive] = useState(2);
+  const [open, setOpen] = useState(false);
+  const [openModel, setOpenModel] = useState(false);
+
+  const { account, userName, connectWallet, createAccount, error } =
+    useContext(ChatAppContect);
+  return (
+    <div className={Style.NavBar}>
+      <div className={Style.NavBar_box}>
+        <div className={Style.NavBar_box_left}>
+          <Image src={images.logo_eth} alt="logo" width={50} height={50} />
+        </div>
+        <div className={Style.NavBar_box_right}>
+          {/* //DESKTOP */}
+          <div className={Style.NavBar_box_right_menu}>
+            {menuItems.map((el, i) => (
+              <div
+                onClick={() => setActive(i + 1)}
+                key={i + 1}
+                className={`${Style.NavBar_box_right_menu_items} ${
+                  active == i + 1 ? Style.active_btn : ""
+                }`}
+              >
+                <Link
+                  className={Style.NavBar_box_right_menu_items_link}
+                  href={el.link}
+                >
+                  {el.menu}
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          {/* //MOBILE */}
+          {open && (
+            <div className={Style.mobile_menu}>
+              {menuItems.map((el, i) => (
+                <div
+                  onClick={() => setActive(i + 1)}
+                  key={i + 1}
+                  className={`${Style.mobile_menu_items} ${
+                    active == i + 1 ? Style.active_btn : ""
+                  }`}
+                >
+                  <Link className={Style.mobile_menu_items_link} href={el.link}>
+                    {el.menu}
+                  </Link>
+                </div>
+              ))}
+
+              <p className={Style.mobile_menu_btn}>
+                <Image
+                  src={images.close}
+                  alt="close"
+                  width={50}
+                  height={50}
+                  onClick={() => setOpen(false)}
+                />
+              </p>
+            </div>
+          )}
+
+          {/* CONNECT WALLET */}
+          <div className={Style.NavBar_box_right_connect}>
+            {account == "" ? (
+              <button onClick={() => connectWallet()}>
+                {""}
+                <span>Connect Wallet</span>
+              </button>
+            ) : (
+              <button onClick={() => setOpenModel(true)}>
+                {""}
+                <Image
+                  src={userName ? images.accountName : images.create2}
+                  alt="Account image"
+                  width={20}
+                  height={20}
+                />
+                {""}
+                <small>{userName || "Create Account"}</small>
+              </button>
+            )}
+          </div>
+
+          <div
+            className={Style.NavBar_box_right_open}
+            onClick={() => setOpen(true)}
+          >
+            <Image src={images.open} alt="open" width={30} height={30} />
+          </div>
+        </div>
+      </div>
+
+      {/* MODEL COMPONENT */}
+      {openModel && (
+        <div className={Style.modelBox}>
+          <Model
+            openBox={setOpenModel}
+            title="Decentralized Messenger"
+            head="Welcome to the Future of Communication"
+            info="Our decentralized messenger revolutionizes the way 
+                  you connect with others, ensuring privacy, security, and freedom. Powered by
+                  blockchain technology, our platform offers end-to-end encryption, peer-to-peer
+                  messaging, and censorship resistance. Say goodbye to centralized surveillance and hello
+                  to true digital sovereignty. Join us in shaping the future of communication."
+            smallInfo="Input your alias name and address"
+            image={images.hero1}
+            functionName={createAccount}
+            address={account}
+          />
+        </div>
+      )}
+      {error == "" ? "" : <Error error={error} />}
+    </div>
+  );
+};
+
+export default NavBar;
